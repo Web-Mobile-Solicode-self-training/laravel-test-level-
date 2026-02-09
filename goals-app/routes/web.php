a1811ef47e4ac493c7ebc81312c137a156eb5561
abdelhaymallouli <abdelhay.mallouli@gmail.com>
2026-02-09 12:41:42 +0000

    feat: Add public index route to new web.php file.

--- goals-app/routes/web.php
+++ goals-app/routes/web.php
@@ -2,14 +2,5 @@
 
 use App\Http\Controllers\GoalController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
 
 Route::get('/', [GoalController::class, 'index'])->name('public.index');
-
-// Admin Routes 
-Route::prefix('admin')->name('admin.goals.')->group(function () {
-    Route::get('/goals', [AdminController::class, 'index'])->name('index');
-    Route::post('/goals/store', [AdminController::class, 'store'])->name('store');
-    Route::get('/goals/{id}/edit', [AdminController::class, 'edit'])->name('edit');
-    Route::delete('/goals/{id}', [AdminController::class, 'destroy'])->name('destroy');
-});
